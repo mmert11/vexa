bd8fd0e8b24b4dd0cf84d63b7b5c87eeed10fa42
mmert11 <rainsensesploit@gmail.com>
2026-08-11 23:29:02 +0300

    build system update

--- xmake.lua
+++ xmake.lua
@@ -2,14 +2,108 @@ set_project("vexa")
 set_version("1.4")
 
 add_rules("mode.debug", "mode.release")
-add_requires("llvm", "bitwuzla", "lief", "quill", "cli11")
 
-option("remill")
-	-- !!!!!!!!!!!!!!!!!!!!!!!!!!
-    set_default(path.join(os.projectdir(), "external", "remill"))
-    set_showmenu(true)
-    set_description("remill path")
+package("remill")
+    set_sourcedir(path.join(os.scriptdir(), "external/remill"))
 
+    on_install(function (package)
+    import("package.tools.cmake")
+
+    local srcdir = package:sourcedir()
+    local depsdir = path.join(srcdir, "dependencies")
+    local depsbuild = path.join(depsdir, "build")
+    local depsinstall = path.join(depsdir, "install")
+
+    local deps_args = {
+        "-G", "Ninja",
+        "-S", depsdir,
+        "-B", depsbuild
+    }
+
+    local remill_args = {
+        "-G Ninja",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCMAKE_PREFIX_PATH=" .. depsinstall
+    }
+
+    if package:is_plat("windows") then
+        table.insert(deps_args, "-DCMAKE_C_COMPILER=clang-cl")
+        table.insert(deps_args, "-DCMAKE_CXX_COMPILER=clang-cl")
+
+        table.insert(remill_args, "-DCMAKE_C_COMPILER=clang-cl")
+        table.insert(remill_args, "-DCMAKE_CXX_COMPILER=clang-cl")
+    else
+        table.insert(deps_args, "-DUSE_EXTERNAL_LLVM=ON")
+    end
+
+    os.vrunv("cmake", deps_args)
+    os.vrunv("cmake", {"--build", depsbuild})
+
+    cmake.install(package, remill_args)
+
+    os.cp(path.join(depsinstall, "include", "*"),
+          path.join(package:installdir(), "include"))
+
+    os.cp(path.join(depsinstall, "lib", "*"),
+          path.join(package:installdir(), "lib"))
+    end)
+package_end()
+
+package("bitwuzla")
+    set_sourcedir(path.join(os.scriptdir(), "external/bitwuzla"))
+
+    add_includedirs("include")
+
+    add_links(
+        "bitwuzla",
+        "bitwuzlabb",
+        "bitwuzlabv",
+        "bitwuzlals"
+    )
+
+    add_syslinks(
+        "mpfr",
+        "gmp"
+    )
+
+    on_install(function (package)
+        local srcdir = package:sourcedir()
+        local builddir = path.join(srcdir, "build-xmake")
+
+        os.vrunv("python3", {
+            path.join(srcdir, "configure.py"),
+            "release",
+            "--static",
+            "--no-testing",
+            "--no-unit-testing",
+            "--no-python",
+            "--prefix=" .. package:installdir(),
+            "-b", builddir
+        }, {curdir = srcdir})
+
+        os.vrunv("ninja", {
+            "-C", builddir,
+            "install"
+        })
+
+        if package:is_plat("linux") then
+            local libdir = path.join(
+                package:installdir(),
+                "lib",
+                "x86_64-linux-gnu"
+            )
+
+            os.cp(
+                path.join(libdir, "*.a"),
+                path.join(package:installdir(), "lib")
+            )
+        end
+    end)
+package_end()
+
+add_requires("llvm", "lief", "quill", "cli11")
+add_requires("bitwuzla", {system = false})
+add_requires("remill", {system = false})
 includes("xmake/vexa_consumer.lua")
 
 target("vexa")
@@ -18,7 +112,7 @@ target("vexa")
     add_rules("utils.install.cmake_importfiles")
     add_files("src/*.cpp", "src/**/*.cpp")
     add_includedirs("include", {public = true})
-    add_packages("llvm", "bitwuzla", "lief", "quill")
+    add_packages("llvm", "bitwuzla", "lief", "quill", "remill")
 
     -- install options
     set_languages("c++20")
@@ -27,8 +121,8 @@ target("vexa")
         os.cp(target:targetfile(), path.join(target:installdir(), "lib"))
     end)
 
-    set_symbols("debug")
-    set_strip("none")
+    --set_symbols("debug")
+    --set_strip("none")
 
 target("vexa-cli")
     set_kind("binary")
@@ -38,8 +132,8 @@ target("vexa-cli")
     configure_vexa_consumer()
     add_packages("cli11")
 
-    set_symbols("debug")
-    set_strip("none")
+    --set_symbols("debug")
+    --set_strip("none")
 
 after_build(function (target)
     import("core.project.task")
--- xmake/vexa_consumer.lua
+++ xmake/vexa_consumer.lua
@@ -3,16 +3,10 @@ function configure_vexa_consumer()
     add_includedirs(path.join(os.projectdir(), "include"))
     add_linkdirs("$(builddir)/$(os)/$(arch)/$(mode)")
 
-    add_packages("vexa", "llvm", "bitwuzla", "lief", "quill")
+    add_packages("vexa", "llvm", "bitwuzla", "lief", "quill", "remill")
 
-	-- !!!!!!!!!!!!!!!!!!!!!!!!!!
-    local remilldir = get_config("remill") or path.join(os.projectdir(), "..", "remill")
-    add_includedirs(path.join(remilldir, "dependencies/install/include"))
-    add_linkdirs(path.join(remilldir, "dependencies/install/lib"))
-
-    add_links(
+    add_linkgroups(
         "vexa",
-        "bitwuzla",
         "remill_arch",
         "remill_arch_x86",
         "remill_arch_aarch64",
@@ -22,12 +16,84 @@ function configure_vexa_consumer()
         "remill_bc",
         "remill_os",
         "remill_version",
-        "decomp", "sla", "slaSupport",
-        "xed", "xed-ild", "glog", "gflags"
+
+        "decomp",
+        "sla",
+        "slaSupport",
+
+        "xed",
+        "xed-ild",
+        "glog",
+        "gflags",
+
+        {group = true}
     )
 
-    add_links("LLVMWindowsManifest", "LLVMXRay", "LLVMLibDriver", "LLVMDlltoolDriver", "LLVMTelemetry", "LLVMTextAPIBinaryReader", "LLVMCoverage", "LLVMLineEditor", "LLVMXCoreDisassembler", "LLVMXCoreCodeGen", "LLVMXCoreDesc", "LLVMXCoreInfo", "LLVMX86TargetMCA", "LLVMX86Disassembler", "LLVMX86AsmParser", "LLVMX86CodeGen", "LLVMX86Desc", "LLVMX86Info", "LLVMWebAssemblyDisassembler", "LLVMWebAssemblyAsmParser", "LLVMWebAssemblyCodeGen", "LLVMWebAssemblyUtils", "LLVMWebAssemblyDesc", "LLVMWebAssemblyInfo", "LLVMVEDisassembler", "LLVMVEAsmParser", "LLVMVECodeGen", "LLVMVEDesc", "LLVMVEInfo", "LLVMSystemZDisassembler", "LLVMSystemZCodeGen", "LLVMSystemZDesc", "LLVMSystemZInfo", "LLVMSPIRVCodeGen", "LLVMSPIRVDesc", "LLVMSPIRVInfo", "LLVMSPIRVAnalysis", "LLVMSparcDisassembler", "LLVMSparcAsmParser", "LLVMSparcCodeGen", "LLVMSparcDesc", "LLVMSparcInfo", "LLVMRISCVTargetMCA", "LLVMRISCVDisassembler", "LLVMRISCVAsmParser", "LLVMRISCVCodeGen", "LLVMRISCVDesc", "LLVMRISCVInfo", "LLVMPowerPCDisassembler", "LLVMPowerPCAsmParser", "LLVMPowerPCCodeGen", "LLVMPowerPCDesc", "LLVMPowerPCInfo", "LLVMNVPTXCodeGen", "LLVMNVPTXDesc", "LLVMNVPTXInfo", "LLVMMSP430Disassembler", "LLVMMSP430CodeGen", "LLVMMSP430Desc", "LLVMMSP430Info", "LLVMMipsDisassembler", "LLVMMipsAsmParser", "LLVMMipsCodeGen", "LLVMMipsDesc", "LLVMMipsInfo", "LLVMLoongArchDisassembler", "LLVMLoongArchAsmParser", "LLVMLoongArchCodeGen", "LLVMLoongArchDesc", "LLVMLoongArchInfo", "LLVMLanaiDisassembler", "LLVMLanaiCodeGen", "LLVMLanaiAsmParser", "LLVMLanaiDesc", "LLVMLanaiInfo", "LLVMHexagonDisassembler", "LLVMHexagonCodeGen", "LLVMHexagonAsmParser", "LLVMHexagonDesc", "LLVMHexagonInfo", "LLVMBPFDisassembler", "LLVMBPFAsmParser", "LLVMBPFCodeGen", "LLVMBPFDesc", "LLVMBPFInfo", "LLVMAVRDisassembler", "LLVMAVRAsmParser", "LLVMAVRCodeGen", "LLVMAVRDesc", "LLVMAVRInfo", "LLVMARMDisassembler", "LLVMARMAsmParser", "LLVMARMCodeGen", "LLVMARMDesc", "LLVMARMUtils", "LLVMARMInfo", "LLVMAMDGPUTargetMCA", "LLVMAMDGPUDisassembler", "LLVMAMDGPUAsmParser", "LLVMAMDGPUCodeGen", "LLVMAMDGPUDesc", "LLVMAMDGPUUtils", "LLVMAMDGPUInfo", "LLVMAArch64Disassembler", "LLVMAArch64AsmParser", "LLVMAArch64CodeGen", "LLVMAArch64Desc", "LLVMAArch64Utils", "LLVMAArch64Info", "LLVMOrcDebugging", "LLVMOrcJIT", "LLVMWindowsDriver", "LLVMMCJIT", "LLVMJITLink", "LLVMInterpreter", "LLVMExecutionEngine", "LLVMRuntimeDyld", "LLVMOrcTargetProcess", "LLVMOrcShared", "LLVMDWP", "LLVMDWARFCFIChecker", "LLVMDebugInfoLogicalView", "LLVMOption", "LLVMObjCopy", "LLVMMCA", "LLVMMCDisassembler", "LLVMDTLTO", "LLVMLTO", "LLVMPlugins", "LLVMPasses", "LLVMHipStdPar", "LLVMCFGuard", "LLVMCoroutines", "LLVMipo", "LLVMVectorize", "LLVMSandboxIR", "LLVMLinker", "LLVMFrontendOpenMP", "LLVMFrontendOffloading", "LLVMObjectYAML", "LLVMFrontendOpenACC", "LLVMFrontendDriver", "LLVMInstrumentation", "LLVMFrontendDirective", "LLVMFrontendAtomic", "LLVMExtensions", "LLVMDWARFLinkerParallel", "LLVMDWARFLinkerClassic", "LLVMDWARFLinker", "LLVMGlobalISel", "LLVMMIRParser", "LLVMAsmPrinter", "LLVMSelectionDAG", "LLVMCodeGen", "LLVMTarget", "LLVMObjCARCOpts", "LLVMCodeGenTypes", "LLVMCGData", "LLVMCAS", "LLVMIRPrinter", "LLVMInterfaceStub", "LLVMFileCheck", "LLVMFuzzMutate", "LLVMScalarOpts", "LLVMInstCombine", "LLVMAggressiveInstCombine", "LLVMTransformUtils", "LLVMBitWriter", "LLVMAnalysis", "LLVMProfileData", "LLVMSymbolize", "LLVMDebugInfoBTF", "LLVMDebugInfoPDB", "LLVMDebugInfoMSF", "LLVMDebugInfoCodeView", "LLVMDebugInfoGSYM", "LLVMDebugInfoDWARF", "LLVMObject", "LLVMTextAPI", "LLVMMCParser", "LLVMIRReader", "LLVMAsmParser", "LLVMMC", "LLVMDebugInfoDWARFLowLevel", "LLVMBitReader", "LLVMFrontendHLSL", "LLVMFuzzerCLI", "LLVMABI", "LLVMCore", "LLVMRemarks", "LLVMBitstreamReader", "LLVMBinaryFormat", "LLVMTargetParser", "LLVMTableGen", "LLVMSupportLSP", "LLVMSupport", "LLVMDemangle")
-    add_links("LLVMSystemZAsmParser", "LLVMMSP430AsmParser")
+    local llvm_links = {
+        "LLVMX86TargetMCA",
+        "LLVMMCA",
+        "LLVMX86Disassembler",
+        "LLVMX86AsmParser",
+        "LLVMX86CodeGen",
+        "LLVMX86Desc",
+        "LLVMX86Info",
+        "LLVMMCDisassembler",
+        "LLVMAsmPrinter",
+        "LLVMPasses",
+        "LLVMIRPrinter",
+        "LLVMHipStdPar",
+        "LLVMCoroutines",
+        "LLVMipo",
+        "LLVMInstrumentation",
+        "LLVMVectorize",
+        "LLVMSandboxIR",
+        "LLVMLinker",
+        "LLVMFrontendOpenMP",
+        "LLVMFrontendDirective",
+        "LLVMFrontendAtomic",
+        "LLVMFrontendOffloading",
+        "LLVMObjectYAML",
+        "LLVMGlobalISel",
+        "LLVMSelectionDAG",
+        "LLVMCodeGen",
+        "LLVMTarget",
+        "LLVMScalarOpts",
+        "LLVMInstCombine",
+        "LLVMObjCARCOpts",
+        "LLVMCodeGenTypes",
+        "LLVMCGData",
+        "LLVMCFGuard",
+        "LLVMAggressiveInstCombine",
+        "LLVMTransformUtils",
+        "LLVMBitWriter",
+        "LLVMAnalysis",
+        "LLVMProfileData",
+        "LLVMSymbolize",
+        "LLVMDebugInfoBTF",
+        "LLVMDebugInfoPDB",
+        "LLVMDebugInfoMSF",
+        "LLVMDebugInfoCodeView",
+        "LLVMDebugInfoGSYM",
+        "LLVMDebugInfoDWARF",
+        "LLVMObject",
+        "LLVMTextAPI",
+        "LLVMMCParser",
+        "LLVMMC",
+        "LLVMDebugInfoDWARFLowLevel",
+        "LLVMFrontendHLSL",
+        "LLVMIRReader",
+        "LLVMBitReader",
+        "LLVMAsmParser",
+        "LLVMCore",
+        "LLVMRemarks",
+        "LLVMBitstreamReader",
+        "LLVMBinaryFormat",
+        "LLVMTargetParser",
+        "LLVMSupport",
+        "LLVMDemangle"
+    }
+
+    add_links(table.unpack(llvm_links))
 
     if is_plat("linux") then
         add_syslinks("z", "zstd", "tinfo", "xml2", "pthread", "dl", "m")
